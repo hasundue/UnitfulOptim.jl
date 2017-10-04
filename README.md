f043a549ba3b8d0b98ad8e3387a47e2b98ed2a90
Shun Ueda <hasundue@gmail.com>
2017-10-04 11:42:02 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # UnitfulOptim
+
+This package enables [Optim.jl](https://github.com/JuliaNLSolvers/Optim.jl) package to handle the Quantity types implemented in [Unitful.jl](https://github.com/ajkeller34/Unitful.jl).
+
+Note that this package has limited functionalities and is not well-tested so far.
+
+## Usage
+
+Currently, univariate optimization is only supported:
+
+```julia
+using Optim
+opt = optimize(f, 1u"m", 5u"m")
+xmin = minimizer(opt)
+fmin = minimum(opt)
+```
+
+The resulting object of optimize() is UnitfulOptimizationResults, which wraps OptimizationResults object used in Optim package. Currently, the supported methods for this object are the followings:
+
+- minimum
+- minimizer
--- src/UnitfulOptim.jl
+++ src/UnitfulOptim.jl
@@ -1,5 +1,37 @@
 module UnitfulOptim
 
-# package code goes here
+using Unitful
+import Unitful: Quantity, Units
+import Optim: OptimizationResults
+import Optim: optimize, minimizer, minimum
+
+struct UnitfulOptimizationResults
+    opt::OptimizationResults
+    xunit::Units
+    funit::Units
+end
+
+function optimize(f, a::Quantity, b::Quantity; kws...)
+    c, d = promote(a, b)
+    optimize(f, c, d; kws...)
+end
+
+function optimize{T<:Real,D,U}(f, a::Quantity{T,D,U}, b::Quantity{T,D,U}; kws...)
+    g(x::T) = ustrip(f(x*U()))
+    opt = optimize(g, ustrip(a), ustrip(b); kws...)
+    UnitfulOptimizationResults(opt, U(), unit(f(a)))
+end
+
+for f in (:minimizer,)
+    @eval function $f(uopt::UnitfulOptimizationResults)
+        $f(uopt.opt) * uopt.xunit
+    end
+end
+
+for f in (:minimum,)
+    @eval function $f(uopt::UnitfulOptimizationResults)
+        $f(uopt.opt) * uopt.funit
+    end
+end
 
 end # module
